025d9166d478bb09c19c969b3d89011e860df0e0
ThantZin-develope <kobhonekyaw123@gmail.com>
2023-03-09 13:33:54 +0630

    _

---
+++ TSS/go.mod
@@ -0,0 +1,5 @@
+module TSS
+
+go 1.19
+
+require github.com/taurusgroup/multi-party-sig v0.6.0-alpha-2021-09-21 // indirect
---
+++ TSS/test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+     "github.com/taurusgroup/multi-party-sig/protocol/cmp/cmp"
+)
+
+
+func main(){
+fmt.Println("Hello");
+}
